0bb87cabeb6451dfcdede093b265c2fd95fd0f5a
Ben Jackson <ben@ben.com>
2024-01-26 11:47:44 -0800

    Update the test driver to report perf. 3090ti, 7B model seeing 75t/s rising to 160t/s aggregate at batch size 8

--- test_with_openai_module.py
+++ test_with_openai_module.py
@@ -15,6 +15,7 @@ client = openai.AsyncOpenAI(base_url="http://localhost:8000/v1", api_key=api_key
 async def request():
     global client
     
+    start_time = asyncio.get_event_loop().time()
     response = await client.chat.completions.create(
         model="bob",
         messages=[
@@ -25,6 +26,8 @@ async def request():
         n=1,
         stop=None,
     )
+    duration = asyncio.get_event_loop().time() - start_time
+    print(f"duration {duration} {response.usage.completion_tokens / duration:.2f} t/s")
     return response.choices[0].message.content
 
 async def main():
@@ -32,7 +35,7 @@ async def main():
     for i in range(20):
         requests.append(request())
     all = await asyncio.gather(*requests)
-    print(all[11])
+    print(all[int(len(all)/2)])
     
 if __name__ == "__main__":
     asyncio.run(main())
